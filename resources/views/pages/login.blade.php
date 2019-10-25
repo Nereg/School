98aa1975ae9573f8380e733627d3ae69cba64cad
nereg <okv23200@gmail.com>
2019-10-25 18:37:44 +0300

    Standalone page done now integrating with backend

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -18,27 +18,56 @@
 .card
 {
   background-color: transparent;
-  box-shadow: 0px;
+  box-shadow: 0 0px 0px 0 rgba(0,0,0,0),0 0px 0px 0 rgba(0,0,0,0);
 }
 .text
 {
-
 color: black;
 font-weight: 500;
 font-size: 45px;
 }
-.card-body
-{
-}
 a
 {
-  color: black;
+  color: #33b5e5 ;
 }
 a:hover
 {
   color: gray;
 }
-
+form
+{
+  color: white;
+}
+.md-form label
+{
+  color: white;
+}
+.form-control
+{
+  color: white;
+}
+.form-control:focus
+{
+  color: white;
+}
+.error
+{
+  color: white;
+  background-color: rgba(250,0,0,.50);
+  box-shadow: 0 2px 5px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12);
+  border-radius: 10px;
+  align-self: center;
+  padding: 10px;
+}
+.good
+{
+  color: white;
+  background-color: rgba(0,250,0,.50);
+  box-shadow: 0 2px 5px 0 rgba(0,0,0,.16),0 2px 10px 0 rgba(0,0,0,.12);
+  border-radius: 10px;
+  align-self: center;
+  padding: 10px;
+}
 </style>
 <div class="main container-fluid img-fluid">
   <!-- Material form login -->
@@ -50,7 +79,12 @@ a:hover
     <h5 class="text-center text">
       Войти
     </h5>
-  
+    <div class="error text-center">
+     Типо ошибка ;)
+    </div>
+    <div class="good text-center">
+      А ту все хорошо
+    </div>
     <!--Card content-->
     <div class="card-body px-lg-5 pt-0">
   
@@ -58,18 +92,15 @@ a:hover
       <form class="text-center" action="#!">
   
         <!-- Email -->
-        <div class="md-form was-validated">
+        <div class="md-form">
           <input type="email" id="materialLoginFormEmail" class="form-control">
-          <label for="materialLoginFormEmail">E-mail</label>
-          <div class="valid-feedback">
-              Looks good!
-            </div>
+          <label for="materialLoginFormEmail">Електронная почта</label>
         </div>
   
         <!-- Password -->
         <div class="md-form">
           <input type="password" id="materialLoginFormPassword" class="form-control">
-          <label for="materialLoginFormPassword">Password</label>
+          <label for="materialLoginFormPassword">Пароль</label>
         </div>
   
         <div class="d-flex justify-content-around">
@@ -77,38 +108,28 @@ a:hover
             <!-- Remember me -->
             <div class="form-check">
               <input type="checkbox" class="form-check-input" id="materialLoginFormRemember">
-              <label class="form-check-label" for="materialLoginFormRemember">Remember me</label>
+              <label class="form-check-label" for="materialLoginFormRemember">Запомнить меня</label>
             </div>
           </div>
           <div>
             <!-- Forgot password -->
-            <a href="">Forgot password?</a>
+            <a href="">Забыли пароль ?</a>
           </div>
         </div>
   
         <!-- Sign in button -->
-        <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Sign in</button>
+        <button class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0" type="submit">Войти</button>
   
         <!-- Register -->
-        <p>Not a member?
-          <a href="">Register</a>
+        <p>Не зарегистрированы ?
+          <a href="">Зарегистрироваться</a>
         </p>
   
         <!-- Social login -->
-        <p>or sign in with:</p>
-        <a type="button" class="btn-floating btn-fb btn-sm">
-          <i class="fab fa-facebook-f"></i>
-        </a>
+        <p>или войти с помощью:</p>
         <a type="button" class="btn-floating btn-tw btn-sm">
-          <i class="fab fa-twitter"></i>
+          <i class="fab fa-google"></i>
         </a>
-        <a type="button" class="btn-floating btn-li btn-sm">
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-        <a type="button" class="btn-floating btn-git btn-sm">
-          <i class="fab fa-github"></i>
-        </a>
-  
       </form>
       <!-- Form -->
   
