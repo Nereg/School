8e981273d36bb07dd6d557e236e3b73e627d9644
nereg <okv23200@gmail.com>
2019-11-27 17:09:14 +0200

    One more edit

--- resources/views/mail/verif.blade.php
+++ resources/views/mail/verif.blade.php
@@ -1,6 +1,6 @@
 @extends('models.MailModel')
 @section('content')
-<h1>Привет, {{$name}}!</h1>
+<h1 class="text-center header">Привет, {{$name}}!</h1>
 
 <a href="{{url('/')}}/activate/{{$Id}}/{{$code}}">Автивировать аккаунт</a>
 @endsection
