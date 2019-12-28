f45fac53e60464602f26c93cf97477f35882fec3
nereg <okv23200@gmail.com>
2019-12-28 12:50:48 +0200

    Yeah ! All secure !

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceSchema('https');// force https {}
+        URL::forceScheme('https');// force https {}
     }
 
     /**
